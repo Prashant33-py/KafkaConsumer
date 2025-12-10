cce8424c8cf407c8948e1feec7f1eac316c9474c
Prashant33-py <pvasala786@gmail.com>
2025-12-10 20:41:37 +0530

    Changes yaml config to java config

---
+++ src/main/java/com/apache/kafka/sample/config/KafkaConsumerConfig.java
@@ -0,0 +1,27 @@
+package com.apache.kafka.sample.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${kafka.group-id}")
+    private String consumerGroupId;
+
+    public Map<String, Object> consumerConfig(){
+        Map<String, Object> consumerProps = new HashMap<>();
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        return consumerProps;
+    }
+}
