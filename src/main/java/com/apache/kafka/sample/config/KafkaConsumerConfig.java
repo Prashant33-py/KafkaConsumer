c30736939f8fd98fcbcbfc7f8be8a2ecaa2f1b43
Prashant33-py <pvasala786@gmail.com>
2025-12-11 18:10:13 +0530

    Changes YAML config to java config for consumer module

--- src/main/java/com/apache/kafka/sample/config/KafkaConsumerConfig.java
+++ src/main/java/com/apache/kafka/sample/config/KafkaConsumerConfig.java
@@ -5,6 +5,11 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -22,6 +27,19 @@ public class KafkaConsumerConfig {
         consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        consumerProps.put(JsonDeserializer.TRUSTED_PACKAGES, "com.apache.kafka.sample.model");
         return consumerProps;
+    }
+
+    public ConsumerFactory<String, Object> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
